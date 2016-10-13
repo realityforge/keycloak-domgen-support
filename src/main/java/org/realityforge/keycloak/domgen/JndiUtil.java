b8bc4aba6bd3b2b0a22747789331bd750aa69a04
Peter Donald <peter@realityforge.org>
2016-10-13 11:10:31 +1100

    Expose method that passes in builder to collect details for context

--- src/main/java/org/realityforge/keycloak/domgen/JndiUtil.java
+++ src/main/java/org/realityforge/keycloak/domgen/JndiUtil.java
@@ -27,7 +27,19 @@ public final class JndiUtil
     throws NamingException
   {
     final JsonObjectBuilder object = Json.createObjectBuilder();
+    buildJsonFromContext( object, context, path );
+    return object.build();
+  }
 
+  /**
+   * Build JsonObject that represents context at specified path. Sub-contexts are represented by
+   * nested JsonObject instances.
+   */
+  public static void buildJsonFromContext( @Nonnull final JsonObjectBuilder builder,
+                                           @Nonnull final Context context,
+                                           @Nonnull final String path )
+    throws NamingException
+  {
     final NamingEnumeration<NameClassPair> enumeration = context.list( path );
     while ( enumeration.hasMore() )
     {
@@ -37,44 +49,42 @@ public final class JndiUtil
       final Object value = context.lookup( subpath );
       if ( null == value )
       {
-        object.addNull( key );
+        builder.addNull( key );
       }
       else if ( String.class.isInstance( value ) )
       {
-        object.add( key, (String) value );
+        builder.add( key, (String) value );
       }
       else if ( Boolean.class.isInstance( value ) )
       {
-        object.add( key, (Boolean) value );
+        builder.add( key, (Boolean) value );
       }
       else if ( Integer.class.isInstance( value ) )
       {
-        object.add( key, (Integer) value );
+        builder.add( key, (Integer) value );
       }
       else if ( Short.class.isInstance( value ) )
       {
-        object.add( key, (Short) value );
+        builder.add( key, (Short) value );
       }
       else if ( Long.class.isInstance( value ) )
       {
-        object.add( key, (Long) value );
+        builder.add( key, (Long) value );
       }
       else if ( Float.class.isInstance( value ) )
       {
-        object.add( key, (Float) value );
+        builder.add( key, (Float) value );
       }
       else if ( Double.class.isInstance( value ) )
       {
-        object.add( key, (Double) value );
+        builder.add( key, (Double) value );
       }
       else if ( Context.class.isInstance( value ) )
       {
         final Context subcontext = (Context) value;
-        object.add( key, buildJsonFromContext( subcontext, subpath ) );
+        builder.add( key, buildJsonFromContext( subcontext, subpath ) );
       }
     }
     enumeration.close();
-
-    return object.build();
   }
 }
